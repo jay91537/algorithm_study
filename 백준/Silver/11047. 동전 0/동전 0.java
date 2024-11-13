284fc18af33a5ffbaa3aa8db427bb824d0adde06
임재현 <veuxft@gmail.com>
2024-11-14 08:33:50 +0900

    [Silver IV] Title: 동전 0, Time: 172 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    //  N 종류의 동전으로 값 K 만들기
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        ArrayList<Integer> number = new ArrayList<>();
+        int count = 0;
+        while (true) {
+            if (count == N) {
+                break;
+            }
+            int input = sc.nextInt();
+            number.add(input);
+            count ++;
+        }
+
+
+        Collections.reverse(number);
+
+        int sum = 0;
+        for( int i= 0; i<N; i++) {
+            if(number.get(i) <= K) {
+                int Q = K / number.get(i);
+                K = K % number.get(i);
+                sum = sum + Q;
+                if (K == 0) break;
+            }
+        }
+
+        System.out.println(sum);
+
+    }
+}
