c660fd7e16ff6bb0fddf122f3a5b651f780441f4
임재현 <veuxft@gmail.com>
2024-11-17 18:51:45 +0900

    [Silver III] Title: 1로 만들기, Time: 188 ms, Memory: 21600 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+//    dp?
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[] temp = new int[N+1];
+
+        int count =0;
+        temp[1] = 0;
+
+        for(int i=2; i<=N; i++){
+            temp[i] = temp[i-1] + 1;
+
+            if(i%2 == 0) {
+                temp[i] = Math.min(temp[i], temp[i/2]+1);
+            }
+            if(i%3 == 0) {
+                temp[i] = Math.min(temp[i], temp[i/3]+1);
+            }
+        }
+
+
+        System.out.println(temp[N]);
+    }
+}
