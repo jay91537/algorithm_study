404c6e634714639b4a489202c37effeb0dc45323
임재현 <veuxft@gmail.com>
2025-01-05 03:04:48 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 73.15 ms, Memory: 97.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,15 +2,29 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-        
-        for(int i=0; i<completion.length; i++) {
-            if(!participant[i].equals(completion[i])) {
-                return participant[i];
+        HashMap <String, Integer> completeMan = new HashMap<>();
+
+        ArrayList<String> participantMan = new ArrayList<>();
+
+        for (int i = 0; i < completion.length; i++) {
+            if(completeMan.containsKey(completion[i])){
+                completeMan.put(completion[i], completeMan.get(completion[i])+1);
+            } else {
+            completeMan.put(completion[i], 1);
             }
         }
-        return participant[participant.length-1];
-        
+
+        participantMan.addAll(Arrays.asList(participant));
+
+        String ans = null;
+        for (String person : participant) {
+            if (completeMan.containsKey(person) && completeMan.get(person) > 0) {
+                completeMan.put(person, completeMan.get(person) - 1);
+            } else {
+                ans = person;
+                break;
+            }
+        }
+        return ans;
     }
 }
