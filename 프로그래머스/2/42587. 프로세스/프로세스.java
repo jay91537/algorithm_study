a969c5275f1cdc78fecd6d171128783cc0ce3e76
임재현 <veuxft@gmail.com>
2025-01-19 17:27:39 +0900

    [level 2] Title: 프로세스, Time: 1.50 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+                int answer = 0;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1);
+        for (int priority : priorities) {
+            pq.offer(priority);
+        }
+
+        while (!pq.isEmpty()) {
+            for (int i = 0; i < priorities.length; i++) {
+                if (priorities[i] == pq.peek()) {
+                    if (i == location) {
+                        return ++answer;
+                    }
+                    pq.poll();
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+    
+}
